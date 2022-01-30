ccb93689ba877ac8618c4bfcfdfa3710f85e26b3
Mikołaj Szymczuk <mikolajszymczuk.dev@wp.pl>
2022-01-30 03:00:25 +0100

    Finish component for mobile version

--- src/components/CalculatorScreen.vue
+++ src/components/CalculatorScreen.vue
@@ -1,13 +1,30 @@
 <template>
-    <div>Screen</div>
+    <!-- Screen component -->
+    <div class="calculator-screen">{{ value }}</div>
 </template>
 
 <script>
 export default {
-    name: "CalculatorScreen"
+    name: "CalculatorScreen",
+    props: {
+        value: {
+            type: Number,
+            required: true
+        }
+    }
 }
 </script>
 
 <style lang="scss" scoped>
+.calculator-screen {
+    padding: 25px 25px 28px 20px;
 
+    background-color: $Very-dark-desaturated-blue-c;
+    border-radius: 10px;
+
+    color: $White;
+    font-size: 2em;
+    font-family: $spartan;
+    text-align: right;
+}
 </style>
